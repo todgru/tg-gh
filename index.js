d556741b056de04bac78d6c987b8cc688ebfcf69
todgru <todgru@gmail.com>
2018-12-07 12:20:00 -0800

    v0.0.1

--- index.js
+++ index.js
@@ -3,17 +3,17 @@
 const got = require("got");
 
 module.exports = opts => {
-  const GH_API = `https://api.github.com/repos/${opts.name}/${opts.repo}`;
+  const GH_REPO_API = `https://api.github.com/repos/${opts.name}/${opts.repo}`;
   const ghopts = {
     json: true,
     headers: {
       accept: "application/vnd.github.v3+json",
-      authorization: `token ${process.env.GITHUB_TOKEN}`,
+      authorization: `token ${process.env.GITHUB_TOKEN || opts.token || opts.gitub_token}`,
     },
   };
 
   async function merges(head, base, commit_message) {
-    const res = await got.post(`${GH_API}/merges`, {
+    const res = await got.post(`${GH_REPO_API}/merges`, {
       ...ghopts,
       ...{ body: { head, base, commit_message } },
     });
@@ -21,7 +21,7 @@ module.exports = opts => {
   }
 
   async function pulls() {
-    const res = await got(`${GH_API}/pulls`, ghopts);
+    const res = await got(`${GH_REPO_API}/pulls`, ghopts);
     return res.body.sort((a, b) => b.number - a.number);
   }
 
--- readme.md
+++ readme.md
@@ -1,3 +1,26 @@
-#tg-gh
+# tg-gh
 
-github helpers
+todgru's Github Helpers
+
+## Usage
+
+A [Github personal access token](https://github.com/settings/tokens/), `GITHUB_TOKEN`, can be set in the environment or `token` can be passed in as one of the options. This token needs `repo` access.
+
+```javascript
+const gh = require("tg-gh")({repo: "my-awesome-repo", name: "todgru", token: "my-token"});
+
+(async () => {
+  // merge master into feature branch
+  const result = await gh.merge("master", "my-feature", "merging master into feature")
+  // => returns status codes,
+  // - 201 for successful update
+  // - 204 for already updated
+  // - 409 merge conflict
+  // - etc.
+
+  // get a list of open PR's
+  const prs = await.gh.pulls()
+  // => returns array of open pr's, or empty array if no pr's.
+
+  })
+```
